29a49b7cd5912001de87d9cbafad24838c9244df
Rumi <rumidalas@gmail.com>
2021-10-14 13:07:20 -0800

    Slick Slider

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(function(){
+    $('#banner').slick({
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        autoplay: false,
+        prevArrow: '<i class="fa fa-chevron-left left"></i>',
+        nextArrow: '<i class="fa fa-chevron-right right"></i>',
+        fade:true,
+        
+      });
+
+});
